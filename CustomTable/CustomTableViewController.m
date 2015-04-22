dfaeb47959b9e891c0e229aee9044a5540cffb3a
Hemant Torsekar <hvt@me.com>
2015-04-22 05:28:16 -0400

    Deletion of cell via animationFade using NSMutableArray model.

--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -23,11 +23,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //Arrays of Recipe Names
     names = [[NSMutableArray alloc]initWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger",@"Ham and Egg Sandwich", @"Creme Brelee",@"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini",nil];
     
+    //Arrays of Recipe Preparation Times
     prepTime = [[NSMutableArray alloc]initWithObjects:@"20 min", @"45 min", @"30 min", @"20 min", @"20 min", @"1 hr",@"1 hr", @"10 min", @"30 min", @"15 min",@"30 min", @"30 min", @"15 min", @"45 min", @"1 hr", @"30 min",nil];
     
-    
+    //Arrays of Recipe Images
     images = [[NSMutableArray alloc]initWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg",@"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg",@"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",@"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg",@"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg",nil];
     
     _recipe = [[NSMutableArray alloc] init];
@@ -50,15 +52,15 @@
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-    {
-        return [_recipe count];
-    }
+{
+    return [_recipe count];
+}
                     
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-    {
-        // Return the number of sections.
-        return 1;
-    }
+{
+    // Return the number of sections.
+    return 1;
+}
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"Cell";
@@ -82,7 +84,7 @@
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryType = UITableViewCellAccessoryCheckmark;
@@ -90,28 +92,23 @@
     rep.isChecked = YES;
 }
 
--(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSIndexPath *oldIndex = [self.tableView indexPathForSelectedRow];
-    Recipe *rep = _recipe[oldIndex.row];
-    rep.isChecked = NO;
-    [tableView reloadData];
-    return indexPath;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:
+//-(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//    NSIndexPath *oldIndex = [self.tableView indexPathForSelectedRow];
+//    Recipe *rep = _recipe[oldIndex.row];
+//    rep.isChecked = NO;
+//    [tableView reloadData];
+//    return indexPath;
+//}
+//
+-(void)tableView:(UITableView *)tableView commitEditingStyle:
 (UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
     [_recipe removeObjectAtIndex:indexPath.row];
-    [tableView reloadData];
-    
+
+    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+
 }
 
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return YES;
-    
-}
 
 
 @end
